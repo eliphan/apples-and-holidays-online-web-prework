844f152ed0b155d421d72e192ff1cea46bf55481
generic-thread-7687 <aliceconnic@gmail.com>
2018-11-09 00:21:02 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -52,11 +52,10 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-  winter_supply = nil
+  
     holiday_hash[:winter].map do |holiday, supplies|
-     winter_supply = supplies
+      supplies
  end
-     winter_supply
 end
 
 def all_supplies_in_holidays(holiday_hash)
