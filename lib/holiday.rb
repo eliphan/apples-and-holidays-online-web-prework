476566945112106d0a808260747232593a4a2574
generic-thread-7687 <aliceconnic@gmail.com>
2018-11-09 00:16:16 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -55,7 +55,6 @@ def all_winter_holiday_supplies(holiday_hash)
     holiday_hash[:winter].map do |holiday, supplies|
      supplies
  end
-   supplies.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
