a3d2229f49bb033962b3a6f471b7e4c35e0cbcea
generic-thread-7687 <aliceconnic@gmail.com>
2018-11-09 00:50:32 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -69,9 +69,10 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
     
     holiday_hash.each do |season, holidays|
-      puts #{season.capitalized}:
-      holidays.each do |holiday, supplies|
-        puts #{holiday.capitalized}
+      puts "#{season.capitalized}:"
+        holidays.each do |holiday, supplies|
+         puts "#{holiday.capitalized}: #{supplies}"
+         
  
 end
 
